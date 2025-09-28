5bddf78247d9aea57a6b17f8e3f77e65dcbf4b7f
Esteban Nuñez <nunezesteban750@gmail.com>
2025-09-27 21:05:01 -0300

    Agrego la estructura de la carpeta client

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>Mueblería Hermanos Jota</h1>
+      <p>¡Bienvenido al nuevo frontend con React y Vite!</p>
+    </div>
+  );
+}
+
+export default App;
